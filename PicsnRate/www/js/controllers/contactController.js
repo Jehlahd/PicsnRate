4966d1764788ea6840528afddba6c7a77f695ecb
Jehlahd <vincent.boudewyn@gmail.com>
2016-03-08 21:11:27 +0100

    Contact + SMS

--- PicsnRate/www/js/controllers/contactController.js
+++ PicsnRate/www/js/controllers/contactController.js
@@ -1,11 +1,15 @@
 angular.module('starter.controllers')
-    .controller('ContactCtrl', function ($scope, $window, $stateParams, $http, $cordovaContacts, $cordovaSms, API_URL) {
+    .controller('ContactCtrl', function ($scope, $window, $stateParams, $http, $cordovaContacts, $cordovaSms, $ionicLoading, API_URL) {
 
         $scope.$on('$ionicView.beforeEnter', function () {
-            // $scope.getAllContacts();
-            $scope.suc = undefined;
-        });
 
+            if ($scope.contacts == undefined) {
+                $ionicLoading.show({
+                    template: '<ion-spinner></ion-spinner><p>Chargement des contacts...</p>'
+                });
+                $scope.getAllContacts();
+            }
+        });
 
         var options = {
             replaceLineBreaks: false, // true to replace \n by a new line, false by default
@@ -16,30 +20,39 @@ angular.module('starter.controllers')
             }
         };
 
-        $scope.sendSMS = function () {
-            $scope.test = true;
+        $scope.sendSMS = function (number) {
 
-            $cordovaSms
-                .send('0633089022', 'This is some dummy text', options)
-                .then(function () {
-                    $scope.suc = true;
-                }, function (error) {
-                    $scope.err = true;
-                    $scope.er = error;
-                    // An error occurred
+            if (number !== undefined) {
+                $ionicLoading.show({
+                    template: '<ion-spinner></ion-spinner><p>Envoie du SMS...</p>'
                 });
+                $cordovaSms
+                    .send(number, "Viens noter ma photo sur PicsnRate", options)
+                    .then(function () {
+                        $ionicLoading.hide();
+                    }, function (error) {
+                        // An error occurred
+                    });
+            }
         }
 
-
+        $scope.filterSecId = function (items) {
+            var result = {};
+            angular.forEach(items, function (value, key) {
+                if (value.phoneNumbers[0] !== undefined) {
+                    result[key] = value;
+                }
+            });
+            return result;
+        }
 
         $scope.getAllContacts = function () {
-
             $cordovaContacts.find({}).then(function (allContacts) { //omitting parameter to .find() causes all contacts to be returned
                 $scope.contacts = allContacts;
-                console.log($scope.contacts);
+                $ionicLoading.hide();
             }, function (err) {
-                console.log(err);
-                $scope.error = err;
+
+                alert("error" + err);
                 // An error occured. Show a message to the user
             })
         };
