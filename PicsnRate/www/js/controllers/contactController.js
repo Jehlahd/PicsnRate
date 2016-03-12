a2887bde8dcdd86aafae03ae9616ec85eb6a056a
Jehlahd <vincent.boudewyn@gmail.com>
2016-03-12 15:44:03 +0100

    Prez

--- PicsnRate/www/js/controllers/contactController.js
+++ PicsnRate/www/js/controllers/contactController.js
@@ -1,20 +1,30 @@
 angular.module('starter.controllers')
-    .controller('ContactCtrl', function ($scope, $window, $stateParams, $http, $cordovaContacts, $cordovaSms, $ionicLoading, API_URL) {
+    .controller('ContactCtrl', function ($scope, $window, $stateParams, $http, $cordovaContacts, $cordovaSms, $ionicLoading, API_URL, $ionicPlatform) {
 
-        $scope.$on('$ionicView.beforeEnter', function () {
-
-            if ($scope.contacts == undefined) {
+        $ionicPlatform.ready(function () {
+            /*$scope.getAllContacts = function () {
+                var options = {};
                 $ionicLoading.show({
                     template: '<ion-spinner></ion-spinner><p>Chargement des contacts...</p>'
                 });
+                $cordovaContacts.find().then(function (allContacts) { //omitting parameter to .find() causes all contacts to be returned
+                    $scope.contacts = allContacts;
+                    $ionicLoading.hide();
+                }, function (err) {
+                    $ionicLoading.hide();
+                    alert("error" + err);
+                    // An error occured. Show a message to the user
+                })
+            };
+            if ($scope.contacts == undefined) {
                 $scope.getAllContacts();
-            }
+            }*/
         });
 
         var options = {
             replaceLineBreaks: false, // true to replace \n by a new line, false by default
             android: {
-                intent: '' // send SMS with the native android SMS messaging
+                intent: 'INTENT' // send SMS with the native android SMS messaging
                     //intent: '' // send SMS without open any other app
                     //intent: 'INTENT' // send SMS inside a default SMS app
             }
@@ -36,25 +46,14 @@ angular.module('starter.controllers')
             }
         }
 
-        $scope.filterSecId = function (items) {
-            var result = {};
-            angular.forEach(items, function (value, key) {
-                if (value.phoneNumbers[0] !== undefined) {
-                    result[key] = value;
-                }
-            });
-            return result;
+        $scope.sendSms = function () {
+
+            $cordovaSms
+                .send("", "Viens noter ma photo sur PicsnRate", options)
+                .then(function () {
+                    $ionicLoading.hide();
+                }, function (error) {
+                    // An error occurred
+                });
         }
-
-        $scope.getAllContacts = function () {
-            $cordovaContacts.find({}).then(function (allContacts) { //omitting parameter to .find() causes all contacts to be returned
-                $scope.contacts = allContacts;
-                $ionicLoading.hide();
-            }, function (err) {
-
-                alert("error" + err);
-                // An error occured. Show a message to the user
-            })
-        };
-
     });
